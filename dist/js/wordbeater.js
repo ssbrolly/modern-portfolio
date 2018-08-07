20078f8a4a2a75e8df841ad5ff35647ea2c21a91
Usong Cho <silvermouse4012@gmail.com>
2018-08-07 10:32:21 -0700

    fade-in pages on load

--- dist/js/wordbeater.js
+++ dist/js/wordbeater.js
@@ -1,28 +1,17 @@
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 
-// Globals
-
-// Available Levels
-const levels = {
-  easy: 5,
-  medium: 3,
-  hard: 1
-};
-
-// To change level
-const currentLevel = levels.medium;
-
-let time = currentLevel;
+// Global vaiables
+let time = 3;
 let score = 0;
 let isPlaying;
 
 // DOM Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
 
 const words = [
   'hat',
@@ -54,73 +43,64 @@ const words = [
 
 // Initialize Game
 function init() {
-  // Show number of seconds in UI
-  seconds.innerHTML = currentLevel;
-  // Load word from array
   showWord(words);
-  // Start matching on word input
-  wordInput.addEventListener('input', startMatch);
-  // Call countdown every second
+  wordInput.addEventListener("input", startMatch)
   setInterval(countdown, 1000);
-  // Check game status
-  setInterval(checkStatus, 50);
-}
+  // setInterval(checkStatus, 50);
+};
 
-// Start match
+// Start Match
 function startMatch() {
   if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = '';
     score++;
-  }
-
-  // If score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
     scoreDisplay.innerHTML = score;
+  } else {
+    isPlaying = false
   }
 }
 
 // Match currentWord to wordInput
 function matchWords() {
   if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Correct!!!';
+    message.innerHTML = "Correct";
     return true;
   } else {
-    message.innerHTML = '';
+    message.innerHTML = "";
     return false;
   }
 }
 
+
 // Pick & show random word
 function showWord(words) {
-  // Generate random array index
   const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
   currentWord.innerHTML = words[randIndex];
-}
+};
 
 // Countdown timer
 function countdown() {
-  // Make sure time is not run out
   if (time > 0) {
-    // Decrement
     time--;
   } else if (time === 0) {
-    // Game is over
     isPlaying = false;
-  }
-  // Show time
+  };
   timeDisplay.innerHTML = time;
-}
+};
 
 // Check game status
 function checkStatus() {
-  if (!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!!!';
-    score = -1;
+  if (time === 0) {
+    message.innerHTML = "Game Over!!!";
+  };
+};
+
+// Keep score
+function keepScore() {
+  if (wordInput === wordInput) {
+    score++;
+    time;
   }
+  scoreDisplay.innerHTML = score;
 }
+
+
