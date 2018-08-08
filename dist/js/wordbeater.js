0f8cb8c58f198fca44c07bcf1deb8a6fa0285d05
Usong Cho <silvermouse4012@gmail.com>
2018-08-07 18:10:29 -0700

    fixed stuttering bug

--- dist/js/wordbeater.js
+++ dist/js/wordbeater.js
@@ -1,7 +1,15 @@
 window.addEventListener("load", init);
 
+const levels = {
+  easy: 4,
+  medium: 2,
+  hard: 1
+}
+
+let currentLevel = levels.medium;
+
 // Global vaiables
-let time = 3;
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
@@ -43,26 +51,34 @@ const words = [
 
 // Initialize Game
 function init() {
+  seconds.innerHTML = currentLevel;
   showWord(words);
-  wordInput.addEventListener("input", startMatch)
+  clearInput();
+  wordInput.addEventListener("input", startMatch);
   setInterval(countdown, 1000);
-  // setInterval(checkStatus, 50);
+  // setInterval(checkStatus, 1000);
 };
 
 // Start Match
 function startMatch() {
   if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = "";
     score++;
-    scoreDisplay.innerHTML = score;
-  } else {
-    isPlaying = false
+  } 
+
+  if (score === -1) {
+    scoreDisplay = 0;
   }
+  scoreDisplay.innerHTML = score;
 }
 
 // Match currentWord to wordInput
 function matchWords() {
   if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = "Correct";
+    message.innerHTML = "Correct!!!";
     return true;
   } else {
     message.innerHTML = "";
@@ -82,10 +98,20 @@ function countdown() {
   if (time > 0) {
     time--;
   } else if (time === 0) {
+    message.innerHTML = "Game Over!!!"
+    score = -1;
+    scoreDisplay.innerHTML = 0;
     isPlaying = false;
-  };
+  } 
   timeDisplay.innerHTML = time;
 };
+
+function clearInput() {
+  if (time === 0) {
+    wordInput.value = "";
+  };
+};
+
 
 // Check game status
 function checkStatus() {
