427b3d5747e353c46f23dda24053891b2ed45007
Usong Cho <silvermouse4012@gmail.com>
2018-08-11 18:07:21 -0700

    fixed bugs

--- dist/js/wordbeater.js
+++ dist/js/wordbeater.js
@@ -1,18 +1,5 @@
 window.addEventListener("load", init);
 
-const levels = {
-  easy: 4,
-  medium: 2,
-  hard: 1
-}
-
-let currentLevel = levels.medium;
-
-// Global vaiables
-let time = currentLevel;
-let score = 0;
-let isPlaying;
-
 // DOM Elements
 const wordInput = document.querySelector("#word-input");
 const currentWord = document.querySelector("#current-word");
@@ -21,97 +8,75 @@ const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
 
+const levels = {
+    easy: 5,
+    medium: 2,
+    hard: 1,
+}
+
+let currentLevel = levels.medium;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
 const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition',
-  'wordbeater',
-  'refrigerator',
-  'knife',
-  'spoon',
-  'induction',
-  'electricity'
-];
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition',
+    'wordbeater',
+    'refrigerator',
+    'knife',
+    'spoon',
+    'induction',
+    'electricity'
+  ];
 
-// Initialize Game
-function init() {
-  seconds.innerHTML = currentLevel;
-  showWord(words);
-  wordInput.addEventListener("input", startMatch, setInterval(countdown, 1000));
-  // setInterval(checkStatus, 1000); 
-    // setInterval(countdown, 1000);
-
-};
-// Start Match
-function startMatch() {
-  if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = "";
-    score++;
-  } 
-  
-  if (score === -1) {
-    scoreDisplay = 0;
-  }
-  scoreDisplay.innerHTML = score;
-}
-
-// Match currentWord to wordInput
-function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = "Correct!!!";
-    return true;
-  } else {
-    message.innerHTML = "";
-    return false;
-  }
-}
-
-
-// Pick & show random word
 function showWord(words) {
-  const randIndex = Math.floor(Math.random() * words.length);
-  currentWord.innerHTML = words[randIndex];
-};
+    let randIndex = Math.floor(Math.random() * words.length);
+    currentWord.innerHTML = words[randIndex];
+}
 
-// Countdown timer
-function countdown() {
-  if (time > 0) {
-    time--;
-  } else if (time === 0) {
-    message.innerHTML = "Game Over!!!"
-    score = -1;
-    scoreDisplay.innerHTML = 0;
-    isPlaying = false;
-    wordInput.value = "";
-  } 
-  timeDisplay.innerHTML = time;
-};
+function init() {
+    seconds.innerHTML = currentLevel;
+    showWord(words);
+    setInterval(countDown, 1000);
+}
 
+function matchInput() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct';
+        return true;
+    }
+}  
 
-
-
+function countDown() {
+    if (time > 0) {
+        time--;
+    } else if (time === 0) {
+        message.innerHTML = "Game Over"
+    }
+    timeDisplay.innerHTML = time;
+}
